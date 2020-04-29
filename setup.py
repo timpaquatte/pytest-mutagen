cba7df34322059205e4bb0c4d0e7b2ac8f2771e0
timpaquatte <timothee.paquatte@polytechnique.edu>
2020-04-29 14:43:25 +0200

    Add some information to setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,33 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-setuptools.setup(
+setup(
     name="pytest-mutagen",
     version="1.0",
-    author="Harrison Goldstein, Timothée Paquatte",
+    author="Timothee Paquatte <timothee.paquatte@polytechnique.edu>, Harrison Goldstein <hgo@seas.upenn.edu>",
     author_email="hgo@seas.upenn.edu",
     description="Add the mutation testing feature to pytest",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hgoldstein95/pytest-mutagen",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        'Topic :: Software Development :: Testing',
+        'Intended Audience :: Developers',
     ],
     entry_points={"pytest11": ["mutagen = plugin", ]},
     python_requires='>=3.6',
+    install_requires=['pytest>=5.4', ],
+    keywords="python testing property-based-testing",
 )
