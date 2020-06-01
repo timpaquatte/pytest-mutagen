484c04a19972077b676afc17ce64de8355373729
timpaquatte <timothee.paquatte@polytechnique.edu>
2020-06-01 16:12:24 +0200

    Add the tests for the mutant_of function

--- tests/test_mutagen.py
+++ tests/test_mutagen.py
@@ -1,5 +1,5 @@
 import pytest_mutagen.mutagen as mg
-from hypothesis import given, strategies as st
+from hypothesis import assume, given, strategies as st
 
 import string
 import os
@@ -29,9 +29,11 @@ def assert_mutant_registry_correct(mutant_name, file_name, with_function_mapping
         assert "f" in func_map
         assert func_map["f"] is empty_function
 
-@pytest.fixture(autouse=True)
-def reset_mutant_registry():
-    yield
+def reset_globals():
+    mg.linked_files.clear()
+    for file, mutants in mg.g_mutant_registry.items():
+        for name, mutant in mutants.items():
+            del mutant
     mg.g_mutant_registry = {mg.APPLY_TO_ALL:{}}
 
 ################
@@ -42,9 +44,39 @@ def reset_mutant_registry():
 def test_mutant_of_correct(mutant_name, file_name):
     mg.mutant_of("f", mutant_name, file=file_name)(empty_function)
     assert_mutant_registry_correct(mutant_name, file_name, True)
+    reset_globals()
 
 @given(WORD)
 def test_mutant_of_no_file(mutant_name):
     mg.mutant_of("f", mutant_name)(empty_function)
     assert_mutant_registry_correct(mutant_name, os.path.basename(__file__), True)
+    reset_globals()
 
+@given(WORD, st.lists(WORD))
+def test_mutant_of_several_files(mutant_name, files):
+    mg.mutant_of("f", mutant_name, files)(empty_function)
+
+    for file in files:
+        assert_mutant_registry_correct(mutant_name, file, True)
+    reset_globals()
+
+@given(WORD, WORD)
+def test_mutant_of_linked_file(mutant_name, file_name):
+    mg.link_to_file(file_name)
+    mg.mutant_of("f", mutant_name)(empty_function)
+    assert_mutant_registry_correct(mutant_name, file_name, True)
+    reset_globals()
+
+@given(WORD, st.lists(WORD, min_size=1), WORD)
+def test_mutant_of_several_linked_files(mutant_name, files, link_file):
+    assume(not link_file in files)
+    for file in files:
+        mg.linked_files[file] = link_file
+
+    mg.mutant_of("f", mutant_name, files)(empty_function)
+    assert_mutant_registry_correct(mutant_name, link_file, True)
+
+    for file in files:
+        assert not file in mg.g_mutant_registry
+
+    reset_globals()
